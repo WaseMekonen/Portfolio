bd00931df8c5a7506aedbea315b3384e0837769f
Wase Mekonen <wase@Wases-MacBook-Air.local>
2022-09-06 11:09:30 +0300

    Updates

---
+++ precache-manifest.4f08b579fe909c2e4cf78a583dfe2666.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "05b0757e127b3712b55c",
+    "url": "/Portfolio/static/css/main.67421cf1.chunk.css"
+  },
+  {
+    "revision": "05b0757e127b3712b55c",
+    "url": "/Portfolio/static/js/main.9da7ec51.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "4052a79b9583a01a8a14",
+    "url": "/Portfolio/static/js/2.4d3fa530.chunk.js"
+  },
+  {
+    "revision": "41858a2ce7466639c93e40b69f53f1aa",
+    "url": "/Portfolio/static/media/portfolioPic.41858a2c.png"
+  },
+  {
+    "revision": "6cd23e1d9c938cec0d1b93ff8d329b53",
+    "url": "/Portfolio/static/media/E-commerce.6cd23e1d.jpg"
+  },
+  {
+    "revision": "c548a534871dc98c7eb072c5b99d7960",
+    "url": "/Portfolio/static/media/Quote-generator.c548a534.jpg"
+  },
+  {
+    "revision": "f7b907de5b6fb001398b21de43ce5afd",
+    "url": "/Portfolio/static/media/library.f7b907de.jpg"
+  },
+  {
+    "revision": "1ecf28d78bcc74e4c9e8a85eb62c99d1",
+    "url": "/Portfolio/static/media/Master-Cooking.1ecf28d7.jpg"
+  },
+  {
+    "revision": "8322b7f06ef0a1805835ca08ae28909a",
+    "url": "/Portfolio/static/media/Flight-Booking.8322b7f0.jpg"
+  },
+  {
+    "revision": "87d04d3e7ca067df4f068d97f6590437",
+    "url": "/Portfolio/static/media/Memory-Game.87d04d3e.jpg"
+  },
+  {
+    "revision": "eb02d8899f8f98749636af182f650b61",
+    "url": "/Portfolio/static/media/github.eb02d889.svg"
+  },
+  {
+    "revision": "eb02d8899f8f98749636af182f650b61",
+    "url": "/Portfolio/static/media/oNewWin.eb02d889.svg"
+  },
+  {
+    "revision": "d75a562fbebde6c18d71effdcb70fd48",
+    "url": "/Portfolio/index.html"
+  }
+];
